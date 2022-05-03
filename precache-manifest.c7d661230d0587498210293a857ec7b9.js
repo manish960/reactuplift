c580e618b49039fd88ba72a484dd8590d0cd042d
Manish <Mani47kumar@gmail.com>
2022-05-03 19:44:56 +0530

    Updates

---
+++ precache-manifest.c7d661230d0587498210293a857ec7b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "760d3ea8a4ad82243240",
+    "url": "/reactuplift/static/js/main.760d3ea8.chunk.js"
+  },
+  {
+    "revision": "5f75bc590bf2a82050fe",
+    "url": "/reactuplift/static/js/1.5f75bc59.chunk.js"
+  },
+  {
+    "revision": "9675125b57405348c1f0",
+    "url": "/reactuplift/static/js/runtime~main.9675125b.js"
+  },
+  {
+    "revision": "5aaa9a66803c13f7dccfc1d7e2bceff3",
+    "url": "/reactuplift/index.html"
+  }
+];
